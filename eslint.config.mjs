181cca1bc3cee2b65adcac2d49c3a93c0dafcf0a
Nano <gaomingzhao666@outlook.com>
2025-04-15 10:42:06 +0900

    chore: turn off `noexplicitany`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,7 @@ const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
   {
     rules: {
-      noexplicitany: 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
     },
   },
 ]
