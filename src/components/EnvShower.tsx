174855604b029111cb78f3d56fa17ff9b72b6ba3
Nano <gaomingzhao666@outlook.com>
2025-04-20 22:01:49 +0900

    chore: add more imgs

--- src/components/EnvShower.tsx
+++ src/components/EnvShower.tsx
@@ -9,6 +9,14 @@ import outsideImg5 from '@/assets/images/outsideOfHouse/img-5.jpg'
 import outsideImg6 from '@/assets/images/outsideOfHouse/img-6.jpg'
 import outsideImg7 from '@/assets/images/outsideOfHouse/img-7.jpg'
 import outsideImg8 from '@/assets/images/outsideOfHouse/img-8.jpg'
+import outsideImg9 from '@/assets/images/outsideOfHouse/img-9.jpg'
+import outsideImg10 from '@/assets/images/outsideOfHouse/img-10.jpg'
+import outsideImg11 from '@/assets/images/outsideOfHouse/img-11.jpg'
+import outsideImg12 from '@/assets/images/outsideOfHouse/img-12.jpg'
+import outsideImg13 from '@/assets/images/outsideOfHouse/img-13.jpg'
+import outsideImg14 from '@/assets/images/outsideOfHouse/img-14.jpg'
+import outsideImg15 from '@/assets/images/outsideOfHouse/img-15.jpg'
+import outsideImg16 from '@/assets/images/outsideOfHouse/img-16.jpg'
 
 const ImagesCarousel = () => {
   const slideData = [
@@ -35,6 +43,30 @@ const ImagesCarousel = () => {
     },
     {
       src: outsideImg8,
+    },
+    {
+      src: outsideImg9,
+    },
+    {
+      src: outsideImg10,
+    },
+    {
+      src: outsideImg11,
+    },
+    {
+      src: outsideImg12,
+    },
+    {
+      src: outsideImg13,
+    },
+    {
+      src: outsideImg14,
+    },
+    {
+      src: outsideImg15,
+    },
+    {
+      src: outsideImg16,
     },
   ]
   return (
